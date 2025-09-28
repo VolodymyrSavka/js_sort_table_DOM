ad7aa3168a6540fde74adf686724a0f61f230042
Volodymyr Savka <savkavoldemar1@gmail.com>
2025-09-28 12:26:01 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,51 @@
 'use strict';
 
-// write code here
+/**
+ * @param {HTMLTableRowElement} row
+ * @param {number} col
+ * @returns {string}
+ */
+const getCellValue = (row, col) => {
+  return row.cells[col].textContent.trim() ?? '';
+};
+
+/**
+ * @param {string} value
+ * @returns {number|string}
+ */
+
+const toNumberIfPossible = (value) => {
+  if (typeof value !== 'string' || !value) {
+    return value;
+  }
+
+  const cleanedValue = value.replace(/[$,\s]/g, '');
+  const number = Number(cleanedValue);
+
+  return isNaN(number) ? value : number;
+};
+
+const constructors = document.querySelectorAll('th');
+const constructorList = [...constructors];
+
+constructorList.forEach((element) => {
+  element.addEventListener('click', () => {
+    const tbody = document.querySelector('tbody');
+    const rows = [...tbody.rows];
+    const col = element.cellIndex;
+
+    rows.sort((rowA, rowB) => {
+      const valueA = toNumberIfPossible(getCellValue(rowA, col));
+      const valueB = toNumberIfPossible(getCellValue(rowB, col));
+
+      if (typeof valueA === 'number' && typeof valueB === 'number') {
+        return valueA - valueB;
+      }
+
+      return String(valueA).localeCompare(String(valueB));
+    });
+
+    tbody.innerHTML = '';
+    rows.forEach((row) => tbody.appendChild(row));
+  });
+});
